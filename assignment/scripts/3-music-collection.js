61f383b43659e3978e901bc4f25ac2af4cbc68ee
Lloyd Prock <lloyd.prock@gmail.com>
2022-08-16 15:34:35 -0500

    completed addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,11 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    const obj = {};
+    obj.albumTitle = title;
+    obj.artist = artist;
+    obj.yearPublished = yearPublished;
+    collection.push(obj);
+    return obj;
+}//end addToCollection
