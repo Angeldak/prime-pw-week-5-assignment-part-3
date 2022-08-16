abffa29ec3899dedcf02f294f658f327de694ded
Lloyd Prock <lloyd.prock@gmail.com>
2022-08-16 15:49:31 -0500

    added albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -9,3 +9,11 @@ function addToCollection(title, artist, yearPublished) {
     collection.push(obj);
     return obj;
 }//end addToCollection
+
+console.log("Adding an album to my collection:", addToCollection("Hybrid Theory", "Linkin Park", 2000));
+console.log("Adding an album to my collection:", addToCollection("Meteora", "Linkin Park", 2003));
+console.log("Adding an album to my collection:", addToCollection("Mansion", "NF", 2015));
+console.log("Adding an album to my collection:", addToCollection("Perception", "NF", 2017));
+console.log("Adding an album to my collection:", addToCollection("Parachutes", "Coldplay", 2000));
+console.log("Adding an album to my collection:", addToCollection("Blink-182", "Blink-182", 2003));
+console.log("Here is my updated collection:", collection);
