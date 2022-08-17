da2d0a79932af412ca79c7e753b058698a819da6
Lloyd Prock <lloyd.prock@gmail.com>
2022-08-16 22:42:27 -0500

    updated addCollection and added 3 tracks each

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,11 +1,12 @@
 console.log('***** Music Collection *****')
 let collection = [];
 
-function addToCollection(title, artist, yearPublished) {
+function addToCollection(title, artist, yearPublished, arrayTracks) {
     const obj = {};
     obj.albumTitle = title;
     obj.artist = artist;
     obj.yearPublished = yearPublished;
+    obj.arrayTracks = arrayTracks;
     collection.push(obj);
     return obj;
 }//end addToCollection
@@ -45,12 +46,12 @@ function showCollection(array) {
     }
 }//end showCollection
 
-console.log("Adding an album to my collection:", addToCollection("Hybrid Theory", "Linkin Park", 2000));
-console.log("Adding an album to my collection:", addToCollection("Meteora", "Linkin Park", 2003));
-console.log("Adding an album to my collection:", addToCollection("Mansion", "NF", 2015));
-console.log("Adding an album to my collection:", addToCollection("Perception", "NF", 2017));
-console.log("Adding an album to my collection:", addToCollection("Parachutes", "Coldplay", 2000));
-console.log("Adding an album to my collection:", addToCollection("Blink-182", "Blink-182", 2003));
+console.log("Adding an album to my collection:", addToCollection("Hybrid Theory", "Linkin Park", 2000, ["Papercut - 3:04", "In the End - 3:36", "Crawling - 3.26"]));
+console.log("Adding an album to my collection:", addToCollection("Meteora", "Linkin Park", 2003, ["Easier to Run - 3:24", "Somewhere I Belong - 3:34", "Breaking the Habit - 3:16"]));
+console.log("Adding an album to my collection:", addToCollection("Mansion", "NF", 2015, ["Mansion - 5:23", "Motivated - 4:21", "Wait - 4:01"]));
+console.log("Adding an album to my collection:", addToCollection("Perception", "NF", 2017, ["Dreams - 3:24", "Remember This - 4:00", "If you want Love - 3:20"]));
+console.log("Adding an album to my collection:", addToCollection("Parachutes", "Coldplay", 2000, ["Yellow - 4:29", "Sparks - 3:47", "Don't Panic - 2:17"]));
+console.log("Adding an album to my collection:", addToCollection("Blink-182", "Blink-182", 2003, ["All the Small Things - 2:48", "Whats my age again? - 2:29", "Adam's Song - 4:28"]));
 console.log("Here is my updated collection:", collection);
 
 showCollection(collection);
@@ -83,14 +84,11 @@ console.log("Testing search with no arg:", search());
 console.log("Testing with LP 2003 Obj:", search(testObject));
 console.log("Testing with Elvis 1955 Obj:", search(testObject2));
 console.log("Testing with NF 2017 Obj:", search(testObject3));
-console.log("Testing with NF 2015 Mansion Obj:", search(testObject4));  //Does not work with additional arguments - Need to refactor
+console.log("NOT WORKING - Testing with NF 2015 Mansion Obj:", search(testObject4));  //Does not work with additional arguments - Need to refactor
 
 
-// Create a function called search. This function should:
+// Add an array of tracks to your album objects. Each track should have a name and duration. You will need to update the functions to support this new property:
 
-// Take an input parameter for a search criteria object. Create your solution based on a search object that has these properties:
-// { artist: 'Ray Charles', year: 1957 }
-// The returned output from search should meet these requirements:
-// Return a new array of all items in the collection matching all of the search criteria.
-// If no results are found, return an empty array.
-// If there is no search object or an empty search object provided as input, then return all albums in the collection.
+// DONE Update the addToCollection function to also take an input parameter for the array of tracks.
+// Update search to allow a trackName search criteria.
+// Update the showCollection function to display the list of tracks for each album with its name and duration.
