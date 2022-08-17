4cbc8b45aa0a9930d9d9cf3f32f874a1048d8f68
Lloyd Prock <lloyd.prock@gmail.com>
2022-08-17 00:06:39 -0500

    refactored search to work with all arguments - stretch complete

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,20 +2,20 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection(title, artist, yearPublished, arrayTracks) {
-    const obj = {};
-    obj.albumTitle = title;
+    const obj = {};  //Initiate object for push
+    obj.albumTitle = title;  //Assigning arguments to object keys
     obj.artist = artist;
     obj.yearPublished = yearPublished;
     obj.arrayTracks = arrayTracks;
-    collection.push(obj);
+    collection.push(obj);  //Adding object to collection
     return obj;
 }//end addToCollection
 
 function findByArtist(artist) {
-    let artistArray = [];
-    for (item in collection) {
-        if (collection[item].artist === artist) {
-            artistArray.push(collection[item]);
+    let artistArray = [];  //Initiating array for return
+    for (item in collection) {  //Begin looping over collection
+        if (collection[item].artist === artist) {  //Checking if collection artist matches artist argument
+            artistArray.push(collection[item]);  //Push to array for return
         }
     }
     return artistArray;
@@ -23,35 +23,41 @@ function findByArtist(artist) {
 
 //searching track names: collection[0].arrayTracks[0].includes("Papercut") === true
 function search(object = {}) {
-    let results = [];
-    let counter;
-    if (Object.values(object).length === 0) {
+    let results = [];  //Initiating Results Array
+    let counter;  //Initiating Counter
+    if (Object.values(object).length === 0) {  //Checking if object argument is blank or not included (defaults to black)
         return collection;
     } else {
-        for (let i = 0; i < Object.keys(collection).length; i++) {
-            counter = 0;
-            for (key in object) {
-                if (collection[i][key] === object[key]) {
-                    counter++;
+        for (let i = 0; i < Object.keys(collection).length; i++) {  //Begins looping over collection
+            counter = 0;  //Resets counter after reach iteration
+            for (key in object) {  //Begins looping over object argument
+                if (Object.keys(object).indexOf("arrayTracks") !== -1) {  //Checks if object argument includes arrayTracks
+                    for (let j = 0; j < collection[i].arrayTracks.length; j++) {  //Begins looping over collection tracks
+                        if (collection[i].arrayTracks[j].includes(object["arrayTracks"])) {  //Checking if collection tracks include object track phrase
+                            counter++;  //Increases counter if track is found
+                        }  //End checking collection tracks
+                    }  //End looping over collection tracks
+                } else if (collection[i][key] === object[key]) {  //If no tracks in object argument then check other object keys against collection
+                    counter++;  //Increase counter if object match is found
                 }
-                if (counter === Object.keys(object).length) {
-                    results.push(collection[i]);
-                }
-            }
-        }
-    }
+                if (counter === Object.keys(object).length) {  //Check is all object arguments were found
+                    results.push(collection[i]);  //If all object arguments found, push to array
+                }  //End check for all-other object arguments
+            }  //End looping over object argument
+        }  //End looping over collection
+    }  //End else
     return results;
 }//end search
 
 function showCollection(array) {
-    let counter;
+    let counter;  //Initiating counter
     console.log(`There are ${array.length} albums in this array:`);
-    for (item in array) {
-        counter = 1;
+    for (item in array) {  //Begin looping over argument array
+        counter = 1;  //Resetting counter after each iteration
         console.log(`${array[item].albumTitle} by ${array[item].artist}, published in ${array[item].yearPublished}`)
-        for (items in array[item].arrayTracks) {
+        for (items in array[item].arrayTracks) {  //Begin looping over tracks in the object array
             console.log(`${counter}. ${array[item].arrayTracks[items]}`);
-            counter++;
+            counter++;  //Increasing counter for bullet point
         }
     }
 }//end showCollection
@@ -93,6 +99,11 @@ const testObject5 = {
     yearPublished: 2000,
     arrayTracks: "Papercut"
 }
+const testObject6 = {
+    artist: "Blink-182",
+    yearPublished: 2003,
+    arrayTracks: "Small Things"
+}
 
 console.log("Testing search with empty Obj:", search({}));
 console.log("Testing search with no arg:", search());
@@ -101,10 +112,4 @@ console.log("Testing with Elvis 1955 Obj:", search(testObject2));
 console.log("Testing with NF 2017 Obj:", search(testObject3));
 console.log("Testing with NF 2015 Mansion Obj:", search(testObject4));
 console.log("Testing with LP 2000 Papercut Obj:", search(testObject5));
-
-
-// Add an array of tracks to your album objects. Each track should have a name and duration. You will need to update the functions to support this new property:
-
-// DONE Update the addToCollection function to also take an input parameter for the array of tracks.
-// Update search to allow a trackName search criteria.
-// DONE Update the showCollection function to display the list of tracks for each album with its name and duration.
+console.log("Testing with Blink-182 2003 Small Things Obj:", search(testObject6));
