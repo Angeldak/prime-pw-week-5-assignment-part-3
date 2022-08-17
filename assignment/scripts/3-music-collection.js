5d31e431987394e54d9d6d8e16fc0c298815c568
Lloyd Prock <lloyd.prock@gmail.com>
2022-08-16 22:28:43 -0500

    Working search stretch goal

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -25,9 +25,17 @@ function search(object = {}) {
     if (Object.values(object).length === 0) {
         return collection;
     } else {
-        console.log(object);
-        return results;
+        for (let i = 0; i < collection.length; i++) {
+            for (let j = 0; j < Object.keys(object).length; j++) {
+                if (collection[i][Object.keys(object)[j]] === object[Object.keys(object)[j]] &&
+                    collection[i][Object.keys(object)[j + 1]] === object[Object.keys(object)[j + 1]] &&
+                    collection[i][Object.keys(object)[j + 1]] !== undefined) {
+                    results.push(collection[i]);
+                }
+            }
+        }
     }
+    return results;
 }//end search
 
 function showCollection(array) {
@@ -46,18 +54,36 @@ console.log("Adding an album to my collection:", addToCollection("Blink-182", "B
 console.log("Here is my updated collection:", collection);
 
 showCollection(collection);
-console.log(" Testing findByArtist Function:", findByArtist("Linkin Park"));
-console.log(" Testing findByArtist Function:", findByArtist("NF"));
-console.log(" Testing findByArtist Function:", findByArtist("Elvis"));
+console.log("Testing findByArtist Function:", findByArtist("Linkin Park"));
+console.log("Testing findByArtist Function:", findByArtist("NF"));
+console.log("Testing findByArtist Function:", findByArtist("Elvis"));
+
+
+//Stretch Goal number one
+const testObject = {
+    artist: "Linkin Park",
+    yearPublished: 2003
+}
+const testObject2 = {
+    artist: "Ray Charles",
+    yearPublished: 1957
+}
+const testObject3 = {
+    artist: "NF",
+    yearPublished: 2017
+}
+const testObject4 = {
+    artist: "NF",
+    yearPublished: 2015,
+    albumTitle: "Mansion"
+}
 
 console.log("Testing search with empty Obj:", search({}));
 console.log("Testing search with no arg:", search());
-
-const testObject = {
-    artist: "Linkin Park",
-    yearPublished: "2003"
-}
 console.log("Testing with LP 2003 Obj:", search(testObject));
+console.log("Testing with Elvis 1955 Obj:", search(testObject2));
+console.log("Testing with NF 2017 Obj:", search(testObject3));
+console.log("Testing with NF 2015 Mansion Obj:", search(testObject4));  //Does not work with additional arguments - Need to refactor
 
 
 // Create a function called search. This function should:
