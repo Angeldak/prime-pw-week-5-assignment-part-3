01ac0e2a9d0014008ee6ff402a2238545064e9b1
Lloyd Prock <lloyd.prock@gmail.com>
2022-08-16 21:21:30 -0500

    search function with only defaults completed

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,13 +10,6 @@ function addToCollection(title, artist, yearPublished) {
     return obj;
 }//end addToCollection
 
-function showCollection(array) {
-    console.log(`There are ${array.length} albums in this array:`);
-    for (item in array) {
-        console.log(`${array[item].albumTitle} by ${array[item].artist}, published in ${array[item].yearPublished}`)
-    }
-}//end showCollection
-
 function findByArtist(artist) {
     let artistArray = [];
     for (item in collection) {
@@ -26,6 +19,23 @@ function findByArtist(artist) {
     }
     return artistArray;
 }//end findByArtist
+
+function search(object = {}) {
+    let results = [];
+    if (Object.values(object).length === 0) {
+        return collection;
+    } else {
+        console.log(object);
+        return results;
+    }
+}//end search
+
+function showCollection(array) {
+    console.log(`There are ${array.length} albums in this array:`);
+    for (item in array) {
+        console.log(`${array[item].albumTitle} by ${array[item].artist}, published in ${array[item].yearPublished}`)
+    }
+}//end showCollection
 
 console.log("Adding an album to my collection:", addToCollection("Hybrid Theory", "Linkin Park", 2000));
 console.log("Adding an album to my collection:", addToCollection("Meteora", "Linkin Park", 2003));
@@ -40,3 +50,21 @@ console.log(" Testing findByArtist Function:", findByArtist("Linkin Park"));
 console.log(" Testing findByArtist Function:", findByArtist("NF"));
 console.log(" Testing findByArtist Function:", findByArtist("Elvis"));
 
+console.log("Testing search with empty Obj:", search({}));
+console.log("Testing search with no arg:", search());
+
+const testObject = {
+    artist: "Linkin Park",
+    yearPublished: "2003"
+}
+console.log("Testing with LP 2003 Obj:", search(testObject));
+
+
+// Create a function called search. This function should:
+
+// Take an input parameter for a search criteria object. Create your solution based on a search object that has these properties:
+// { artist: 'Ray Charles', year: 1957 }
+// The returned output from search should meet these requirements:
+// Return a new array of all items in the collection matching all of the search criteria.
+// If no results are found, return an empty array.
+// If there is no search object or an empty search object provided as input, then return all albums in the collection.
